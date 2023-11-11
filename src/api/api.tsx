794b67e999dd21b2179a41fb251788bc3a4f00ed
Behruz01 <ibragimovbehruz822@gmail.com>
2023-11-11 16:50:35 +0500

    change_home_blog

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,18 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const baseUrlImg = 'http://165.227.164.31:4040';
-export const baseUrl = 'http://165.227.164.31:4040/api';
-const token =localStorage.getItem('token');
+export const baseUrlImg = "http://165.227.195.35:3333/";
+export const baseUrl = "http://165.227.195.35:3333/api";
+const token = localStorage.getItem("token");
 
 const apiRoot = axios.create({
-	baseURL: `http://161.35.188.153`,
+  baseURL: `http://161.35.188.153`,
 });
 
 const instance = axios.create({
-	baseURL: 'http://165.227.195.35:3333/api',
-	headers:{
-		Authorization: `Bearer ${token} `
-	}
+  baseURL: "http://165.227.195.35:3333/api",
+  headers: {
+    Authorization: `Bearer ${token} `,
+  },
 });
 
 // // Interceptors for handling common scenarios
--- src/components/HeroCarousel/Carousel.jsx
+++ src/components/HeroCarousel/Carousel.jsx
@@ -1,94 +1,93 @@
-import { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Slider from 'react-slick';
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import Slider from "react-slick";
 
-import vector from '../../assets/images/Vector.png';
+import vector from "../../assets/images/Vector.png";
 
 export default class Partner extends Component {
-	render() {
-		const settings = {
-			dots: false,
-			infinite: true,
-			speed: 500,
-			slidesToShow: 5,
-			slidesToScroll: 1,
-			rtl: true,
-			autoplay: true,
-			centerMode: true,
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      rtl: true,
+      autoplay: true,
+      centerMode: true,
       responsive: [
         {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 420,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1
-        }
-      }
-    ]
-		};
-		return (
-			<div className=' my-[20px] '>
-				
-				<Slider {...settings}>
-					<div className=' w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   '>
-						<div className=' w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center'>
-							LOGO
-						</div>
-					</div>
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 420,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 1,
+          },
+        },
+      ],
+    };
+    return (
+      <div className=" my-[20px] ">
+        <Slider {...settings}>
+          <div className=" w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   ">
+            <div className=" w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center">
+              LOGO
+            </div>
+          </div>
 
-					<div className=' w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   '>
-						<div className=' w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center'>
-							LOGO
-						</div>
-					</div>
-					<div className=' w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   '>
-						<div className=' w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center'>
-							LOGO
-						</div>
-					</div>
-					<div className=' w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   '>
-						<div className=' w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center'>
-							LOGO
-						</div>
-					</div>
-					<div className=' w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   '>
-						<div className=' w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center'>
-							LOGO
-						</div>
-					</div>
-					<div className=' w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   '>
-						<div className=' w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center'>
-							LOGO
-						</div>
-					</div>
-          <div className=' w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   '>
-						<div className=' w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center'>
-							LOGO
-						</div>
-					</div>
-          <div className=' w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   '>
-						<div className=' w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center'>
-							LOGO
-						</div>
-					</div>
-				</Slider>
-			</div>
-		);
-	}
+          <div className=" w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   ">
+            <div className=" w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center">
+              LOGO
+            </div>
+          </div>
+          <div className=" w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   ">
+            <div className=" w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center">
+              LOGO
+            </div>
+          </div>
+          <div className=" w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   ">
+            <div className=" w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center">
+              LOGO
+            </div>
+          </div>
+          <div className=" w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   ">
+            <div className=" w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center">
+              LOGO
+            </div>
+          </div>
+          <div className=" w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   ">
+            <div className=" w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center">
+              LOGO
+            </div>
+          </div>
+          <div className=" w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   ">
+            <div className=" w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center">
+              LOGO
+            </div>
+          </div>
+          <div className=" w-[197px] h-[100px] bg-[#F6F7ED]  rounded-[16px]   ">
+            <div className=" w-full h-full text-[#C1C2B4] text-[40px] font-semibold  flex items-center justify-center">
+              LOGO
+            </div>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
 }
